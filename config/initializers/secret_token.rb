7f77f15420204fbde71fd306f58aabd45eb68b44
mohamed rachidi <rachidi29@gmail.com>
2014-05-03 19:21:11 -0700

    first push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BoringFriends::Application.config.secret_token = '1d402c20873f8703d0eecd0741c9b707dafa821e5196191611ea58bc9f66d2f5599a4c35f1810983a219ed5d33212a541854808c79e5befc0950f6d46f5e9801'
